e1435aebb3b1bbf4d3d44a0ed35e01865a0f6d4b
savin8305 <kanhaji0402@gmail.com>
2023-10-23 00:37:25 +0530

    modified app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://shop-dot.vercel.app/',
   credentials: true
 }));
 
