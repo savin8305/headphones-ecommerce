c0c067af6116ef48ae75b7041e9c75115f75e890
savin8305 <kanhaji0402@gmail.com>
2023-10-23 08:31:32 +0530

    some changes

--- frontend/src/components/Products/SuggestedProduct.jsx
+++ frontend/src/components/Products/SuggestedProduct.jsx
@@ -5,30 +5,33 @@ import styles from "../../styles/styles";
 import ProductCard from "../Route/ProductCard/ProductCard";
 
 const SuggestedProduct = ({ data }) => {
-  const { allProducts } = useSelector((state) => state.products);
-  const [productData, setProductData] = useState([]);
+  const {allProducts} = useSelector((state) => state.products);
+  const [productData,setProductData] = useState();
 
   useEffect(() => {
-    const filteredProducts =
-      allProducts && allProducts.filter((product) => product.category === data.category);
-    setProductData(filteredProducts);
+    const d =
+    allProducts && allProducts.filter((i) => i.category === data.category);
+    setProductData(d);
   }, []);
 
   return (
     <div>
-      {data && (
-        <div className={`${styles.section} p-4`}>
-          <h2 className={`${styles.heading} text-2xl font-semibold border-b mb-5`}>
-            Related Products
+      {data ? (
+        <div className={`p-4 ${styles.section}`}>
+          <h2
+            className={`${styles.heading} text-[25px] font-[500] border-b mb-5`}
+          >
+            Related Product
           </h2>
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-6 lg:grid-cols-4 lg:gap-8 xl:grid-cols-5 xl:gap-10 mb-12">
-             {productData &&
-               productData.map((product) => (
-                 <ProductCard data={product} key={product.id} />
-               ))}
-          </div>
+          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+             {
+                productData && productData.map((i,index) => (
+                    <ProductCard data={i} key={index} />
+                ))
+             }
+      </div>
         </div>
-      )}
+      ) : null}
     </div>
   );
 };
